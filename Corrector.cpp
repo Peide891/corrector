b9bc57a58d8561aada13c9057a3da0d0928f08aa
Peide891 <sirpley891@gmail.com>
2025-05-23 23:59:58 +0200

    Add files via upload

--- Corrector.cpp
+++ Corrector.cpp
@@ -51,34 +51,110 @@ using namespace std;
 	               while( getline(input, line)){
 					   stringstream ss(line);
                        string par;
-                       while(ss>>line){
-						   insercio(par);
-						   esborra(par);
-						   substitucio(par);
-						   transposicio(par);
+                       while(ss>>par){
+						  pair<int, bool> consig=conte_signes(par);
+						  string original=par;
+						  char sig;
+						   if(consig){
+							   sig=par[consig.first-1];
+							   original= par.substr(0, paraula.size() - 1);
+						   }
+						   int mida=consig.first;
+						   insercio(par, mida);
+						   esborra(par, mida);
+						   substitucio(par, mida);
+						   transposicio(par, mida);
+						   string change=getWord(par);
+						   //no tene signes
+						   if(change!=original){
+							   v.push_back(make_pair(original, change));
+						   }
+						   if(consig){
+						      change+=sig;
+						   }
+							   if (!out.empty()) {
+									out += " ";
+							   }
+							   else{
+									out += paraula;
+							   }
+							
 					   }
+					   escriureCorrecsio(rutaOutput);
 				   }
-	                
-					//programar els cambis (funcions auxilliars):
-					//-> agregar(incercio), eliminar, sustituir i tranposicio,  
-					
-					
+				    
+					bolcaRegistre(rutaLog);
 				}
 		    }
 		}
 }
+		pair<int, bool> conte_signes(string &paraula){
+			pair<int, bool> find= make_pair(paraula.size(),false);
+				if(paraula[paraula.size()-1]<64){ // valor de ?=(63)(max) a !=(33)(min) en ascii 
+			        find.second=true;
+			        find.first=paraula.size()-1;
+			    }
+			    return find;
+		}
 		
-		void insercio(string &paraula){
-			
+		string getWord(string &paraula){
+			string res=paraula;
+			if(not corret.empty()){
+				if(corret.size()==1){
+					res=corret[0];
+				}
+				else{
+					int j=0;
+					int Mfreq=0;
+					for (int i = 0; i <=corret.size(); ++i) {
+					  int f=dic_bst.getFrequencia(corret[i]);
+					   if(Mfreq<f){
+						   Mfreq=f;
+						   j=i;
+					   }
+					}
+					res=corret[j];
+				}
+			}
+			return res;
 		}
-	    void esborra(string &paraula){
-			
+		
+		void insercio(string &paraula, int &mida){
+			for (int i = 0; i <=mida; ++i) {
+                 for (char c = 'a'; c <= 'z'; ++c) {
+					string nou = paraula;
+					nou.insert(i, 1, c);
+					if(dic_bts.conte(nou)){
+					   	corret.push_back(nou);
+					}
+                 }
+                 
+            }
+            /*paraula=getWord(paraula);
+            if (hiHaSignes){
+		        paraula += paraula[mida];
+	        }*/
 		}
-		void substitucio(string &paraula){
-			
+	    void esborra(string &paraula, int &mida){
+			for (int i = 0; i <=mida; ++i) {
+					string nou = paraula;
+					nou.erase(i,1);
+            }
 		}
-		void transposicio(string &paraula){
-			
+		void substitucio(string &paraula, int &mida){
+			for (int i = 0; i <=mida; ++i) {
+                 for (char c = 'a'; c <= 'z'; ++c) {
+					string nou = paraula;
+					nou[i]=c;
+                 }
+            }
+		}
+		void transposicio(string &paraula, int &mida){
+			for (int i = 0; i <=mida-1; ++i) {
+				string nou = paraula;
+			    swap(nova[i], nova[i + 1]);
+			    
+			}
 		}
 		//...
 		
@@ -86,15 +162,30 @@ using namespace std;
 		//*********************************************************
 		//Lectura i escriptura
 		//*********************************************************
+void Corrector::escriureCorrecsio(const string &rutaOutput)
+{
+    ofstream fitxerOut(rutaOutput);
+	
+	 if (not fitxerOut.is_open()) {
+        cerr << "Error obrint el fitxer: " << rutaOutput << endl;
+    }
+    else{
+		fitxerOut << out << endl;
+    }
+}
 
 //*********************************************************
 void Corrector::bolcaRegistre(const string &rutaLog)
 {
     ofstream fitxerLog(rutaLog);
 	
-	// escriure el contingut de l'estructura que emmagatzema
-	// els registres a fitxerLog
-	
-	// fitxerLog << original << " -> " << corregida << endl;
+	 if (not fitxerLog.is_open()) {
+        cerr << "Error obrint el fitxer: " << rutaLog << endl;
+    }
+    else{
+		for (int i = 0; i < unsigned int (v.size()); ++i) {
+        fitxerLog <<v[i].first<< " -> " <<v[i].second  <<endl;;
+       }
+    }
 }
 //*********************************************************
