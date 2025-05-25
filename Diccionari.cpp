957a942c3c392c9353d755548a7016daba461aa9
Peide891 <sirpley891@gmail.com>
2025-05-25 18:41:22 +0200

    Add files via upload

--- Diccionari.cpp
+++ Diccionari.cpp
@@ -8,16 +8,12 @@ using namespace std;
  //*********************************************************
  // Constructors
  //*********************************************************
-  Diccionari(){}
-  Diccionari(vector<ParFreq> &parf, BTS<ParFreq> &t) {
-	  diccionari=parf;
-	  bts=t;
-  }
+   Diccionari::Diccionari(){}
  //...	   
  //*********************************************************
  // Destructor
  //*********************************************************
- ~Diccionari(){}
+  Diccionari::~Diccionari(){}
  //...
  //*********************************************************
  // Modificadors
@@ -26,58 +22,67 @@ using namespace std;
  /* Post: Si la paraula del parell rebut per  paràmetre no 
  apareixia ja al diccionari, s'ha afegit al diccionari el
 parell rebut per paràmetre; altrament, el diccionari no  s'ha modificat */
-void Diccionari::insereix(const ParFreq &pf){
+
+/*void Diccionari::insereix(const ParFreq &pf){
 	bool find=false;
 	int i=0;
-	while( i<unsigned int(diccionari.size() and not find)){
+	int mida=diccionari.size();
+	while( i<(mida) and not find){
 		if(diccionari[i]!=pf){
-			diccionari.push_back(pf);
-			find=true;
+				find=true;
+			}
+			++i;
 		}
-		++i;
-	}
-}
-void Diccionari::ordre(){
-	sort(diccionari.begin(), diccionari.end());
-    insereix(0 ,diccionari.size()-1);
-}
+		if(find){
+		diccionari.push_back(pf);
+    }
+}*/
 void Diccionari::insereix(int inici, int fi){
 	if(inici<= fi){
 		int mig = inici + (fi - inici) / 2; 
-		bts.insert(diccionari[mig]);
+		bst.insert(diccionari[mig]);
 		insereix(inici, mig - 1);  // part esquerra del vector
         insereix(mig + 1, fi);     // part dret del vector
 	}
 		
 }
+void Diccionari::ordre(){
+	sort(diccionari.begin(), diccionari.end());
+    insereix(0 ,diccionari.size()-1);
+}
+
  /* Pre:  Cert  */
   /* Post: El resultat indica si el diccionari conté la
   paraula rebuda per paràmetre */
  bool Diccionari::conte(const string &paraula) const {// cerca en el BST
-	bool res=bst.find(paraula).first;
-    return res;
+	ParFreq pf(paraula, 0);
+	pair<bool, ParFreq> res=bst.find(pf);
+	
+    return res.first;
  }
  /* Pre: La paraula rebuda per paràmetre està en el	diccionari */
   /* Post: El resultat és la freqüència que apareix al
 	diccionari de la paraula rebuda per paràmetre */	 
    int Diccionari::getFrequencia(const string &paraula) const{
-	 int f;
-	 bool find=false;
-	 int i=0;
-	 while(i<unsigned int(diccionari.size())){
-		if(diccionari[i]==paraula){
-			f=diccionari[i].getFrequencia();
-		}
-		++i;
-     }
-     return f; 
-   }		  
+    ParFreq pf(paraula, 0);
+    pair<bool, ParFreq> res = bst.find(pf);
+    int freq=0;
+    if (res.first) {
+        freq = res.second.getFreq();
+    }
+    return freq;
+}		  
 
-// ...
-
-// ...
 
 //*********************************************************
+void Diccionari::escriu(){
+	for (int i = 0; i < diccionari.size(); ++i) {
+        cout << "Palabra: " << diccionari[i].getPar()
+                  << " | Frecuencia: " << diccionari[i].getFreq() << '\n';
+    }
+    cout<<"fin"<<endl;
+}
+
 void Diccionari::llegeixDeFitxer(const string &path)
 {
     ifstream fitxer(path);
@@ -90,9 +95,11 @@ void Diccionari::llegeixDeFitxer(const string &path)
     while (fitxer >> paraula >> frequencia) {  // llegeix un parell (paraula, freq) 
 											   // per línia
 		ParFreq pf(paraula, frequencia);
-        insereix(pf);       
+		//cout<<pf.getPar()<<pf.getFreq()<<endl;
+        diccionari.push_back(pf);       
     }
     ordre(); //insereix al BTS
+    //escriu();
 }
 
 	  
